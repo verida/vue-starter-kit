fd9464d46fe636c8d6d15a2eacc97d28a61201fb
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-02-09 14:58:14 +0100

    update env file

--- src/views/SSOLogin.vue
+++ src/views/SSOLogin.vue
@@ -4,14 +4,14 @@
     :onSuccess="onSuccess"
     :contextName="contextName"
     :logo="logo"
-    loginText="Verida Vue Starter Kit"
+    :loginText="loginText"
   />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-const { VUE_APP_CONTEXT_NAME, VUE_APP_LOGO } = process.env;
+const { VUE_APP_CONTEXT_NAME, VUE_APP_LOGO, VUE_APP_LOGIN_TEXT } = process.env;
 
 export default defineComponent({
   name: "Connect",
@@ -23,6 +23,7 @@ export default defineComponent({
       error: null,
       contextName: VUE_APP_CONTEXT_NAME,
       logo: VUE_APP_LOGO,
+      loginText: VUE_APP_LOGIN_TEXT,
     };
   },
   methods: {
